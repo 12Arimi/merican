2c9f5d82d5674f675763512bdcad5a670892d6d3
Arimi <gitimuarimi@gmail.com>
2025-12-21 13:59:24 +0300

    updated next.config.ts file to correct the redirection for AI crawlers and Search Engines

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        // If anyone (or any bot) hits the bare domain
+        source: '/',
+        destination: '/en',
+        permanent: true, // This sends the 308 code bots love
+      },
+    ];
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'lxvghczvmslyiiyrpzaw.supabase.co',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
